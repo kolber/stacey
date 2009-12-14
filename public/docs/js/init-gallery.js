075677e4db07c751d26d035311f91ff53175825a
Anthony Kolber <ae@aestheticallyloyal.com>
2009-12-15 08:53:05 +1100

    Template cleanup - added RSS link to footer - converted gallery js unobtrusive inclusion to be clearer

--- public/docs/js/init-gallery.js
+++ public/docs/js/init-gallery.js
@@ -3,13 +3,16 @@ $(function() {
 	// don't init gallery if it consists of a single photo
 	if($("div.image").length > 1) {
 		// wrap images
-		$("p#project-count").replaceWith('<p id="gallery-count" class="col one"><em>&#8470;</em> <span>1/1</span></p>');
-		$('<div id="gallery-navigation" class="col three"><p><a href="#" id="next-image">Next image</a> <em>&rarr;</em></p><p><a href="#" id="previous-image">Previous image</a> <em>&larr;</em></p></div>').insertAfter("p#gallery-count");
 		$("div.image").wrapAll("<div id='image-wrapper'><div id='image-holder'></div></div>");
+		$("div.image").show();
 		
 		// init gallery: Gallery.init(imageHolder, imageWrapperWidth, imageCountHolder, nextButton, prevButton)
 		Gallery.init($("div#image-holder"), 560, $("p#gallery-count").children("span")[0], $("a#next-image"), $("a#previous-image"));
-		
-		$("div.image img").show();
+
+	} else {
+	  // hide gallery count and navigation
+	  $("p#gallery-count, div#gallery-navigation").hide();
+	  // show project navigation
+	  $("p#project-count").show();
 	}
 });
