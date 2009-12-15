afc2cb064965d6e7a59e4dc8852da96c7802e254
Anthony Kolber <ae@aestheticallyloyal.com>
2009-12-15 19:21:00 +1100

    Missed one of the regexes with trailing /u flags

--- app/page-data.inc.php
+++ app/page-data.inc.php
@@ -87,7 +87,7 @@ Class PageData {
 			$split_url = explode("/", $page->url_path);
 		$page->slug = $split_url[count($split_url) - 1];
 		# @page_name
-		$page->page_name = ucfirst(preg_replace('/[-_](.)/eu', "' '.strtoupper('\\1')", $page->data['@slug']));
+		$page->page_name = ucfirst(preg_replace('/[-_](.)/e', "' '.strtoupper('\\1')", $page->data['@slug']));
 		# @root_path
 		$page->root_path = Helpers::relative_root_path();
 		# @thumb
