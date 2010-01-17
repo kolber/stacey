40856801e758d43e78993e4d14f64d5bb02aec06
Anthony Kolber <ae@aestheticallyloyal.com>
2010-01-17 19:59:25 +1100

    Forgot an echo...

--- app/stacey.inc.php
+++ app/stacey.inc.php
@@ -7,7 +7,6 @@ Class Stacey {
 	function handle_redirects() {
 		# rewrite any calls to /index or /app back to /
 		if(preg_match('/^\/?(index|app)\/?$/', $_SERVER['REQUEST_URI'])) {
-  		echo $_SERVER['REQUEST_URI'].'<br>';
 			header('HTTP/1.1 301 Moved Permanently');
 			header('Location: ../');
 			return true;
