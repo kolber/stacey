4cc609dc755cc36eb78c71c8ceb6b0656d23b56a
splendid <someone@somethingsplendid.com>
2010-09-07 18:27:57 +1000

    added captioning support, allowing descriptions to be added on a per-asset basis
    when creating asset collections, stacey will look for a plain text file named
    "captions.stacey". this file contains key/value pairs in the same format as
    other stacey content files. the key should be the name of the file you wish
    to save a caption for, and with the value being either plain text or markdown.

--- app/asset-types/asset.inc.php
+++ app/asset-types/asset.inc.php
@@ -10,6 +10,7 @@ Class Asset {
   function __construct($file_path) {
     # create and store data required for this asset
     $this->set_default_data($file_path);
+    $this->set_caption($file_path);
   }
   
   function construct_link_path($file_path) {
@@ -28,6 +29,33 @@ Class Asset {
     $this->data['@url'] = $this->link_path;
     $this->data['@file_name'] = $this->file_name;
     $this->data['@name'] = ucfirst(preg_replace(array('/[-_]/', '/\.[\w\d]+?$/', '/^\d+?\./'), array(' ', '', ''), $this->file_name));
+  }
+  
+  function set_caption($file_path) {
+    # store contents of caption file (if it exists, otherwise, pass back an empty string)
+    $caption_file_path = dirname($file_path).'/captions.stacey';
+    $caption_data = (file_exists($caption_file_path)) ? file_get_contents($caption_file_path) : '';
+    
+    # get caption_data into a parsable state
+    $caption_data = "\n" . $caption_data .= "\n-\n";
+		# remove UTF-8 BOM and marker character in input, if present
+    $caption_data = preg_replace('/^\xEF\xBB\xBF|\x1A/', '', $caption_data);
+		# standardize line endings
+		$caption_data = preg_replace('/\r\n?/', "\n", $caption_data);
+		
+		# pull out each key/value pair from the content file
+		preg_match_all('/(?<=\n)([0-9\.a-z\d_\-]+?:[\S\s]*?)\n\s*?-\s*?\n/', $caption_data, $matches);
+		
+		# iterate over stored captions, to see if there is one for this asset
+		foreach($matches[1] as $match) {
+			# split the string by the first colon
+			$colon_split = explode(':', $match, 2);
+
+		  # if key matches file name, store caption
+      if (trim($colon_split[0]) == $this->file_name) {
+         $this->data['@caption'] = (strpos($colon_split[1], "\n") === false) ? trim($colon_split[1]) : Markdown(trim($colon_split[1]));
+      }
+		}		    
   }
   
 }
